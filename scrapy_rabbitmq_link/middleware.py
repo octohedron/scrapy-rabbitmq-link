36b645fefe9673660ddd25191935950238d1a1d4
Mantas Briliauskas <m.brilauskas@gmail.com>
2016-08-25 22:41:26 +0300

    Add rabbitmq message acks in spider stats

--- scrapy_rabbitmq_link/middleware.py
+++ scrapy_rabbitmq_link/middleware.py
@@ -1,4 +1,5 @@
 import pika
+import logging
 
 RABBITMQ_ACKNOWLEDGE_ON_RESPONSE_STATUS = [200, 404]
 
@@ -8,13 +9,39 @@ class RabbitMQMiddleware(object):
 
     def __init__(self, settings): 
         self.ack_status = settings.get('RABBITMQ_ACKNOWLEDGE_ON_RESPONSE_STATUS', RABBITMQ_ACKNOWLEDGE_ON_RESPONSE_STATUS)
-    
+        self.init = True
+
     @classmethod
     def from_settings(self, settings):
         return RabbitMQMiddleware(settings)
 
+    def ensure_init(self, spider):
+	if  self.init:
+	    self.server = spider.crawler.engine.slot.scheduler.queue.server
+	    self.stats = spider.crawler.stats
+	    self.init = False
+
+    def inc_stat(self, stat, spider):
+	    self.stats.inc_value('scheduler/acking/%(stat)s/rabbitmq' % {'stat': stat},
+		    spider=spider)
+
     def process_response(self, request, response, spider):
-        if  response.status in self.ack_status:
-            server = spider.crawler.engine.slot.scheduler.queue.server
-            server.basic_ack(delivery_tag=request.meta.get('delivery_tag'))
-        return response
+        self.ensure_init(spider)
+
+        if  response.status in self.ack_status and not is_a_picture(request):
+            self.server.basic_ack(delivery_tag=request.meta.get('delivery_tag'))
+            logging.info('Acked (%(status)d): %(url)s' %
+		        {'url': request.url, 'status': response.status})
+            self.inc_stat('acked', spider)
+        elif is_a_picture(request):
+            logging.info('Picture (%(status)d): %(url)s',
+		        {'url': request.url, 'status': response.status})
+            self.inc_stat('picture', spider)
+        else:
+            logging.info('Unacked (%(status)d): %(url)s',
+		        {'url': request.url, 'status': response.status})
+            self.inc_stat('unacked', spider)
+            return response
+
+def is_a_picture(request):
+    return request.url.endswith('.jpg')
