d0794dbab13230fd91c2d5275766a105466cea9c
Mantas Briliauskas <m.briliauskas@gmail.com>
2017-02-18 19:35:41 +0200

    Increase version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='scrapy-rabbitmq-link',
     author='Mantas Briliauskas',
     description='RabbitMQ Plug-in for Scrapy',
-    version='0.2.0',
+    version='0.2.1',
     author_email='m.briliauskas@gmail.com',
     license='MIT',
     url='https://github.com/mbriliauskas/scrapy-rabbitmq-link',
